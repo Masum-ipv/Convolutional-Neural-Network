01881914780ff5eb566c4edbfee6a8b5309ba3ed
Masum-ipv <fakhrulislamcuet@gmail.com>
2018-09-25 17:06:17 +0600

    implement upto backpropagation

--- CNN from scratch/run.py
+++ CNN from scratch/run.py
@@ -1,7 +1,9 @@
 from skimage import data, io, color
 import numpy
+import math, random
 from utils import conv, relu, pooling
 from grapical_view import show
+from neural_network import backpropagate
 
 '''
 The major steps involved are as follows:
@@ -47,9 +49,7 @@ l1_filter[1, :, :] = numpy.array([[[1,   1,  1],
                                    [0,   0,  0],
 
                                    [-1, -1, -1]]])
-# print(l1_filter.shape)
-
-
+# print(l1_filter)
 
 '''---------------------- Convolutional Layer 1 ---------------------------'''
 l1_feature_map = conv(img, l1_filter)
@@ -80,5 +80,42 @@ l3_feature_map_relu_pool = pooling(l3_feature_map_relu, 2, 2)
 print("l3_feature_map_relu_pool", l3_feature_map_relu_pool.shape)
 print("**End of conv layer 3**\n\n")
 
-'''---------------------- Graphing results ---------------------------'''
-show(img, l1_feature_map, l1_feature_map_relu, l1_feature_map_relu_pool, l2_feature_map, l2_feature_map_relu, l2_feature_map_relu_pool, l3_feature_map, l3_feature_map_relu, l3_feature_map_relu_pool)
+'''---------------------- Graphing results of convolution ---------------------------'''
+# show(img, l1_feature_map, l1_feature_map_relu, l1_feature_map_relu_pool, l2_feature_map, l2_feature_map_relu, l2_feature_map_relu_pool, l3_feature_map, l3_feature_map_relu, l3_feature_map_relu_pool)
+
+
+
+
+'''---------------------- Fully Connected layer ---------------------------'''
+print("**Fully connected layer(Convolutional layer to Fully connected layer**")
+fc = l3_feature_map_relu_pool.reshape(-1)
+print(fc.shape)
+
+inputs = [fc]
+
+targets = [[1 if i == j else 0 for i in range(10)]
+               for j in range(10)]
+print(inputs)
+print(targets)
+
+random.seed(0)   # to get repeatable results
+input_size = len(fc)  # each input is a vector of length 49
+num_hidden = 5   # we'll have 5 neurons in the hidden layer
+output_size = 10 # we need 10 outputs for each input
+
+# each hidden neuron has one weight per input, plus a bias weight
+hidden_layer = [[random.random() for __ in range(input_size + 1)]
+                for __ in range(num_hidden)]
+
+# each output neuron has one weight per hidden neuron, plus a bias weight
+output_layer = [[random.random() for __ in range(num_hidden + 1)]
+                for __ in range(output_size)]
+
+# the network starts out with random weights
+network = [hidden_layer, output_layer]
+
+# 100 iterations seems enough to converge // 10000
+for __ in range(10000):
+    for input_vector, target_vector in zip(inputs, targets):
+        backpropagate(network, input_vector, target_vector)
+

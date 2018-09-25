a4018afab612c3c5d91047883e6bd39f1203d284
Masum-ipv <fakhrulislamcuet@gmail.com>
2018-09-25 12:58:51 +0600

    Implement convlutional layer

--- CNN from scratch/run.py
+++ CNN from scratch/run.py
@@ -1,7 +1,7 @@
 from skimage import data, io, color
 import numpy
-from matplotlib import pyplot as plt
-from utils import conv
+from utils import conv, relu, pooling
+from grapical_view import show
 
 '''
 The major steps involved are as follows:
@@ -51,6 +51,34 @@ l1_filter[1, :, :] = numpy.array([[[1,   1,  1],
 
 
 
-'''---------------------- Convolutional Layer ---------------------------'''
+'''---------------------- Convolutional Layer 1 ---------------------------'''
 l1_feature_map = conv(img, l1_filter)
-print(l1_feature_map)
+print("l1_feature_map", l1_feature_map.shape)
+l1_feature_map_relu = relu(l1_feature_map)
+print("l1_feature_map_relu", l1_feature_map_relu.shape)
+l1_feature_map_relu_pool = pooling(l1_feature_map_relu, 2, 2)
+print("l1_feature_map_relu_pool", l1_feature_map_relu_pool.shape)
+print("**End of conv layer 1**\n\n")
+
+'''---------------------- Convolutional Layer 2 ---------------------------'''
+l2_filter = numpy.random.rand(3, 5, 5, l1_feature_map_relu_pool.shape[-1])
+l2_feature_map = conv(l1_feature_map_relu_pool, l2_filter)
+print("l2_feature_map", l2_feature_map.shape)
+l2_feature_map_relu = relu(l2_feature_map)
+print("l2_feature_map_relu", l2_feature_map_relu.shape)
+l2_feature_map_relu_pool = pooling(l2_feature_map_relu, 2, 2)
+print("l2_feature_map_relu_pool", l2_feature_map_relu_pool.shape)
+print("**End of conv layer 2**\n\n")
+
+'''---------------------- Convolutional Layer 3 ---------------------------'''
+l3_filter = numpy.random.rand(1, 7, 7, l2_feature_map_relu_pool.shape[-1])
+l3_feature_map = conv(l2_feature_map_relu_pool, l3_filter)
+print("l3_feature_map", l3_feature_map.shape)
+l3_feature_map_relu = relu(l3_feature_map)
+print("l3_feature_map_relu", l3_feature_map_relu.shape)
+l3_feature_map_relu_pool = pooling(l3_feature_map_relu, 2, 2)
+print("l3_feature_map_relu_pool", l3_feature_map_relu_pool.shape)
+print("**End of conv layer 3**\n\n")
+
+'''---------------------- Graphing results ---------------------------'''
+show(img, l1_feature_map, l1_feature_map_relu, l1_feature_map_relu_pool, l2_feature_map, l2_feature_map_relu, l2_feature_map_relu_pool, l3_feature_map, l3_feature_map_relu, l3_feature_map_relu_pool)
